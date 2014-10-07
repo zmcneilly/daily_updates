7e5e9f3a149604fa060b1222f7027e92106d9f32
Zach McNeilly <zach.mcneilly@gmail.com>
2014-10-07 14:20:25 -0400

    Works as intended

--- daily_updates.py
+++ daily_updates.py
@@ -1,11 +1,15 @@
 import json
 import urllib.request
 import hashlib
+import smtplib
 from datetime import datetime, timedelta
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 SENDER = "zach.mcneilly@gmail.com"
-SMTP = "smtp.google.com"
-PASS = "SOME PASS"
+SMTP = "smtp.gmail.com:587"
+# Add a variable "PASS" here with your gmail password
+# I.e. PASS = 'password'
 
 
 class EmailAlerts(object):
@@ -16,6 +20,8 @@ class EmailAlerts(object):
         '''The basic constructor method.'''
         self.recp = recipient
         self.sender = SENDER
+        self.smtp = SMTP
+        self.password = PASS
         self.notifications = []
 
     def add_notify(self, site):
@@ -28,7 +34,7 @@ class EmailAlerts(object):
         result = False
         for site in self.notifications:
             cut_off = datetime.now() - timedelta(hours=23)
-            if 'last' in site and \
+            if 'last' not in site or \
                 datetime.strptime(site['last'], "%Y%m%dT%H:%M") < cut_off:
                 result = True
             else:
@@ -38,6 +44,19 @@ class EmailAlerts(object):
     def send_notifications(self):
         '''This method will create an email and send it with links to the
         updated content.'''
+        text = ('From: {}\n'.format(self.sender) + 
+                'To: {}\n'.format(self.recp) +
+                'Subject: Daily Updates\n\n')
+
+        for site in self.notifications:
+            text += "{} has been updated!\n\t{}".format(site['name'],
+                                                        site['website'])
+        smtp = smtplib.SMTP(self.smtp)
+        smtp.starttls()
+        smtp.login(self.sender, self.password)
+        smtp.sendmail(self.sender, self.recp, text)
+        smtp.quit()
+
 
 
 
@@ -47,6 +66,7 @@ def main():
     try:
         json_file = open('sites.json', 'r')
         sites = json.load(json_file)
+        email = EmailAlerts(SENDER)
         json_file.close()
     except IOError:
         print("No sites.json file!")
@@ -59,11 +79,15 @@ def main():
         if 'md5' in sites[site] and site_md5 == sites[site]['md5']:
             pass
         else:
-            add_notify(sites[site])
+            email.add_notify(dict(sites[site]))
             sites[site]['last'] = datetime.now().strftime("%Y%m%dT%H:%M")
-        sites[site]['md5'] = md5
+        sites[site]['md5'] = site_md5
         json_file = open('sites.json', 'w')
-        json_file.write(json.dump(sites, sort_keys=True,
+        json_file.write(json.dumps(sites, sort_keys=True,
                         indent=4, separators=(',', ': ')))
         json_file.close()
+    if email.can_notify():
+        email.send_notifications()
 
+if __name__ == "__main__":
+    main()
